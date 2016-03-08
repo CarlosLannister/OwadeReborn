f2e6601b26d83a97ed88bb23434ab89c74753e5f
Carlos Lannister <carloslannister@gmail.com>
2016-03-08 11:45:54 +0100

    Clean crispy forms

--- owade/forms.py
+++ owade/forms.py
@@ -8,17 +8,3 @@ class LoginForm(forms.Form):
     user_username = forms.CharField(label="Username")
     user_password = forms.CharField(label="Password", widget=forms.PasswordInput())
 
-class AnalyzeForm(forms.Form):
-    image = forms.CharField(label='Select an image', help_text='dd format')
-
-
-    helper = FormHelper()
-    helper.form_class = 'form-horizontal'
-    helper.layout = Layout(
-        Field('image', css_class='input-xlarge'),
-        'radio_buttons',
-        FormActions(
-            Submit('save_changes', 'Save changes', css_class="btn-primary"),
-            Submit('cancel', 'Cancel'),
-        )
-    )
